b88f89d09b7b2be4eab856556e1929d99c46a5df
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2024-09-26 20:53:04 +0530

    Create InsertAtBeginning.cpp

---
+++ Insertion in singly/InsertAtBeginning.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtBeg(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    Node *head;
+    head = node1;
+
+    insertAtBeg(head, 12);
+    
+
+    insertAtBeg(head, 16);
+    print(head);
+}
