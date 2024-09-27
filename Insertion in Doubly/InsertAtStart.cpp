d18f80605f7e02ff66a92536756097bd4be2919f
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2024-09-27 23:43:48 +0530

    Update InsertAtStart.cpp

--- Insertion in Doubly/InsertAtStart.cpp
+++ Insertion in Doubly/InsertAtStart.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    head = head->next;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void InsertAtStart(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
+int main()
+{
+    Node *node1 = new Node(3);
+    Node *head = node1;
+
+    cout << "this is the list after insertion  :";
+    InsertAtStart(head, 22);
+    InsertAtStart(head, 23);
+    InsertAtStart(head, 24);
+    InsertAtStart(head, 25);
+    InsertAtStart(head, 26);
+
+    print(head);
+}
 
